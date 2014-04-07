b8a744fc03d11531a36889627b67c7c6e12ed446
John Machahuay <john.cfmr.2009@gmail.com>
2014-04-07 17:43:47 -0500

    Added marker and type for marker

--- app/scripts/controllers/mapa.js
+++ app/scripts/controllers/mapa.js
@@ -1,36 +1,47 @@
 'use strict';
 
 angular.module('myAppAngularApp')
-	.controller('MapaCtrl', function ($scope) {
+	.controller('MapaCtrl', function ($scope, $http) {
 		//Google maps
 		$scope.completeLoatedMap = false;
+		$scope.points = '';
 	    $scope.loadMap = function(){
-	    	
-	        setTimeout(function(){
-	            var lating = null,
-	                coordinates = '',
-	                marker=undefined;
-	            if(lating==null || lating == '') {
-	                lating = "-34.397, 150.644";
-	            }
-	            coordinates=lating.split(',');
+	    	$http.get('../../musica_arte.json')
+    		.success(function(resp) {
+    			$scope.points = resp.result;
 
-	            var mapOptions = {
+    			var mapOptions = {
 	                zoom: 12,
-	                center: new google.maps.LatLng(coordinates[0], coordinates[1])
+	                center: new google.maps.LatLng(-12.063025, -77.035855)
 	            };
 	            var map = new google.maps.Map(document.getElementById('gmaps-canvas'),
 	                mapOptions);
-	            if(lating != ''){
-	                var myLatlng = new google.maps.LatLng(coordinates[0], coordinates[1]);
+
+	            var infowindow = new google.maps.InfoWindow();
+	            var marker, i, icon;
+	            
+
+    			for(var j=1; j<=$scope.points.length;j++) {
+    				if($scope.points[j][8] == '1')
+    					icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+    				if($scope.points[j][8] == '2')
+    					icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+    				if($scope.points[j][8] == '3')
+    					icon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
 	                marker = new google.maps.Marker({
-	                    position: myLatlng,
+	                    position: new google.maps.LatLng($scope.points[j][6], $scope.points[j][7]),
 	                    map: map,
-	                    title: 'My Museum'
+	                    icon: icon
 	                });
-	            }
-	            $scope.completeLoatedMap = true;
-	        }, 500);
+	                google.maps.event.addListener(marker, 'click', (function(marker, j) {
+				        return function() {
+			          		infowindow.setContent($scope.points[j][3]);
+				          	infowindow.open(map, marker);
+			        	}
+			      	})(marker, j));
+		            $scope.completeLoatedMap = true;
+		        }
+    		});
 	    }
 
 	    $scope.loadMap();
