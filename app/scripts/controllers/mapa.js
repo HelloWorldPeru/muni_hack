a20a06a87dc4b3598d877f729458b0391fcb1103
jonathancg90 <jonathancg90@gmail.com>
2014-04-12 18:03:50 -0500

    change points

--- app/scripts/controllers/mapa.js
+++ app/scripts/controllers/mapa.js
@@ -4,11 +4,12 @@ angular.module('myAppAngularApp')
 	.controller('MapaCtrl', function ($scope, $http) {
 		//Google maps
 		$scope.completeLoatedMap = false;
-		$scope.points = '';
+		$scope.school = '';
+		$scope.hola = 'hola';
 	    $scope.loadMap = function(){
 	    	$http.get('../../musica_arte.json')
     		.success(function(resp) {
-    			$scope.points = resp.result;
+    			$scope.school = resp.result;
 
     			var mapOptions = {
 	                zoom: 12,
@@ -21,27 +22,40 @@ angular.module('myAppAngularApp')
 	            var marker, i, icon;
 	            
 
-    			for(var j=1; j<=$scope.points.length;j++) {
-    				if($scope.points[j][8] == '1')
-    					icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
-    				if($scope.points[j][8] == '2')
-    					icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
-    				if($scope.points[j][8] == '3')
-    					icon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
-	                marker = new google.maps.Marker({
-	                    position: new google.maps.LatLng($scope.points[j][6], $scope.points[j][7]),
-	                    map: map,
-	                    icon: icon
-	                });
-	                google.maps.event.addListener(marker, 'click', (function(marker, j) {
-				        return function() {
-			          		infowindow.setContent($scope.points[j][3]);
-				          	infowindow.open(map, marker);
-			        	}
-			      	})(marker, j));
-		            $scope.completeLoatedMap = true;
+    			for(var j=1; j<=$scope.school.length;j++) {
+    				try{
+    					if($scope.school[j][8] == '1')
+	    					icon = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
+	    				if($scope.school[j][8] == '2')
+	    					icon = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+	    				if($scope.school[j][8] == '3')
+	    					icon = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
+		                marker = new google.maps.Marker({
+		                    position: new google.maps.LatLng($scope.school[j][6], $scope.school[j][7]),
+		                    map: map,
+		                    icon: icon
+		                });
+		                google.maps.event.addListener(marker, 'click', (function(marker, j) {
+					        return function() {
+				          		infowindow.setContent($scope.school[j][3]);
+					          	infowindow.open(map, marker);
+				        	}
+				      	})(marker, j));
+			            $scope.completeLoatedMap = true;
+    				} catch(err){
+
+    				}
 		        }
     		});
+	    }
+
+
+	    $scope.loadSchool = function(){
+
+	    }
+
+	    $scope.loadEvents = function(){
+
 	    }
 
 	    $scope.loadMap();
